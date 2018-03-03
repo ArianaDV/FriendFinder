14bd7f07eccbf6736639c039507522da75623cf9
Ariana <ariana.dv4@gmail.com>
2018-03-03 07:42:03 -0600

    can push name and photo link to friendsData array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,14 @@
-var friendsArray = [
+
+var friendsData = [
     {
-        name: "",
-        photo: "",
-        scores: []
+        name: "Tommy Thompson",
+        photo: "https://s-media-cache-ak0.pinimg.com/originals/e9/7e/32/e97e32608a057c6e63278a108d984649.jpg",
+        scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+        name: "Sally Sanders",
+        photo: "https://i.pinimg.com/736x/d5/7a/e1/d57ae1e0abaa478e79388007b6d6dd09--woman-face-woman-style.jpg",
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
     }];
 
-var user1 = [].reduce();
-var user2 = [].reduce();
-
-friendsArray.forEach(friend => {
-    if (user1.scores === user2.scores) {
-        res.render("index",friend);
-    }
-})
+module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,22 @@
+
+var friendsData = require('../data/friends.js');
+
 module.exports = function (app) {
 app.get("/api/friends", function (req, res) {
-    var friends = req.params.friends;
-
-    if (friends) {
-        console.log(friends);
-
-        for (var i = 0; i < friends.length; i++) {
-            if (friends === friends[i]) {
-                return res.json(friends[i].name);
-            }
-        }
-        return res.json(false);
-    }
-    return res.json(friends);
+    res.json(friendsData);
+    
 });
 
-// Create New Characters - takes in JSON input
+
 app.post("/api/friends", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
+    
     var newFriend = req.body;
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    var scoresArray = [];
+    
+    friendsData.push(newFriend);
 
-    console.log(newFriend);
+    });
 
-    characters.push(newFriend);
+}
 
-    res.json(newFriend);
-});
 
-};
